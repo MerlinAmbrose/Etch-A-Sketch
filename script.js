d222f3dfb1e86af76ea0bd760d6b89f4237b981c
Merlin <batinicajames@gmail.com>
2020-07-30 06:23:39 -0700

    etch finished

---
+++ script.js
@@ -0,0 +1,28 @@
+const container = document.getElementById("container");
+
+function makeGrid() {
+    let e = document.getElementById('e').value;
+    container.style.setProperty('--grid-rows', e);
+    container.style.setProperty('--grid-cols', e);
+    for (c = 0; c < (e * e); c++) {
+      let cell = document.createElement("div");
+      container.appendChild(cell).className = "grid-item";
+      cell.addEventListener('mouseover', e => e.target.classList.add('color'))
+    };
+  };
+  let test = document.querySelector('.gridcontainer');
+        test.addEventListener('mouseover', function(event) {
+            event.target.remove();
+        })
+
+
+  
+  
+
+
+
+
+
+
+
+
